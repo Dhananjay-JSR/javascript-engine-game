61ef3b2a5e252d564ec7df95c9ab5ddb34819411
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-04-17 15:05:42 +0530

    PreCaching Scene

--- src/assets/Main_Scene.js
+++ src/assets/Main_Scene.js
@@ -1,7 +1,16 @@
 import {Scene} from Phaser;
+import zero from './assets/0.png';
+import one from './assets/1.png';
+import two from './assets/2.png';
+import three from './assets/3.png';
+import four from './assets/4.png';
+import five from './assets/5.png';
+import six from './assets/6.png';
+import seven from './assets/7.png';
+import eight from './assets/8.png';
+import nine from './assets/9.png';
 
-
-class Main_Scene extends Phaser.Scene
+class Main_Scene extends Scene
 {
     constructor ()
     {
@@ -11,8 +20,20 @@ class Main_Scene extends Phaser.Scene
 
     preload ()
     {
+        this.load.image('0', zero);
+        this.load.image('1', one);
+        this.load.image('2', two);
+        this.load.image('3', three);
+        this.load.image('4', four);
+        this.load.image('5', five);
+        this.load.image('6', six);
+        this.load.image('7', seven);
+        this.load.image('8', eight);
+        this.load.image('9', nine);
     }
     create(){
 
     }
 }
+
+export default Main_Scene;
