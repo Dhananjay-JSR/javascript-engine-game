54c28017549ef980b283311ec5485ac04884a64c
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-04-17 15:04:15 +0530

    Scene Created

---
+++ src/assets/Main_Scene.js
@@ -0,0 +1,18 @@
+import {Scene} from Phaser;
+
+
+class Main_Scene extends Phaser.Scene
+{
+    constructor ()
+    {
+        super()
+
+    }
+
+    preload ()
+    {
+    }
+    create(){
+
+    }
+}
